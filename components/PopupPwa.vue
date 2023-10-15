1eddbe1227628b027cd4ef674d2a9f14aae40a89
fariidnugraha <farid@jojonomic.com>
2023-10-16 00:10:59 +0700

    edit config pwa

--- components/PopupPwa.vue
+++ components/PopupPwa.vue
@@ -3,23 +3,33 @@
 <template>
   <ClientOnly>
     <div
-      v-if="$pwa?.needRefresh"
+      v-if="$pwa?.offlineReady || $pwa?.needRefresh"
       class="fixed bottom-5 right-5 z-[100] rounded-lg border border-gray-300/70 bg-white p-4 shadow dark:border-gray-700 dark:bg-background-dark-soft"
       role="alert"
     >
-      <p class="mb-2 text-sm text-yami dark:text-slate-200 md:text-base">
-        New content available, click on reload button to update.
-      </p>
+      <div class="mb-2 text-sm text-yami dark:text-slate-200 md:text-base">
+        <span v-if="$pwa.offlineReady"> App ready to work offline </span>
+        <span v-else> New content available, click on reload button to update. </span>
+      </div>
 
       <UButton
+        v-if="$pwa.needRefresh"
         color="red"
         label="Reload"
+        class="mr-2"
         @click="$pwa.updateServiceWorker()"
+      />
+
+      <UButton
+        color="gray"
+        label="Close"
+        @click="$pwa.cancelPrompt()"
       />
     </div>
 
+    v-if="$pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh"
     <div
-      v-if="$pwa?.showInstallPrompt && !$pwa?.needRefresh"
+      v-if="$pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh"
       class="fixed bottom-5 right-5 z-[100] rounded-lg border border-gray-300/70 bg-white p-4 shadow dark:border-gray-700 dark:bg-background-dark-soft"
       role="alert"
     >
